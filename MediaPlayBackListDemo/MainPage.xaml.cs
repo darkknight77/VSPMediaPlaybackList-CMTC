767bf7a158e95d0485d1148041291241afaa484e
darkknight77 <shaiklatheefmrf98@gmail.com>
2020-03-25 23:46:35 +0530

    Added : Play, Pause, Previous, Next, Rewind, FastForward, SkipBack, SkipForward, Shuffle, Auto Repeat, AspectRatio,  FullScreenToggle

--- MediaPlayBackListDemo/MainPage.xaml.cs
+++ MediaPlayBackListDemo/MainPage.xaml.cs
@@ -27,7 +27,7 @@ namespace MediaPlayBackListDemo
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        MediaPlaybackItem playbackItem;
+        
         MediaPlayer mediaplayer;
         MediaPlaybackList playbackList = new MediaPlaybackList();
 
@@ -62,7 +62,7 @@ namespace MediaPlayBackListDemo
 
         }
 
-
+        
         private async void PickMultiFile(object sender, RoutedEventArgs e)
         {
 
@@ -103,8 +103,6 @@ namespace MediaPlayBackListDemo
             }
                 
                 
-                
-
             
         }
 
@@ -138,6 +136,8 @@ namespace MediaPlayBackListDemo
 
         async private void PlaybackSession_PlaybackStateChanged(MediaPlaybackSession sender, object args)
         {
+            Debug.WriteLine($"Current playback state changed to: {sender.PlaybackState}");
+
             await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
             {
                 var currentState = sender.PlaybackState;
@@ -152,18 +152,116 @@ namespace MediaPlayBackListDemo
         }
         private void UpdateTransportControls(MediaPlaybackState state)
         {
+            Debug.WriteLine($"Enabling Prev/Next Buttons");
             nextBtn.IsEnabled = true;
             previousBtn.IsEnabled = true;
             if (state == MediaPlaybackState.Playing)
             {
                 playButtonSymbol.Symbol = Symbol.Pause;
+                Debug.WriteLine($"Media playing");
             }
             else
             {
                 playButtonSymbol.Symbol = Symbol.Play;
+                Debug.WriteLine($"Media paused");
+            }
+        }
+        
+        private void skipBackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            mediaplayer.PlaybackSession.Position -= TimeSpan.FromSeconds(10);
+        }
+
+        private void rewindBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (mediaplayer.PlaybackSession.PlaybackRate <= 1 && mediaplayer.PlaybackSession.PlaybackRate >= 0.1)
+                mediaplayer.PlaybackSession.PlaybackRate -= 0.1;
+            else mediaplayer.PlaybackSession.PlaybackRate -= 0.5;
+            Debug.WriteLine($"PlaybackRate : {mediaplayer.PlaybackSession.PlaybackRate}x");
+        }
+
+        private void previousBtn_Click(object sender, RoutedEventArgs e)
+        {
+            playbackList.MovePrevious();
+        }
+
+        private void playpauseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (MediaPlaybackState.Playing == mediaplayer.PlaybackSession.PlaybackState)
+            {
+                mediaplayer.Pause();
+            }
+            else if (MediaPlaybackState.Paused == mediaplayer.PlaybackSession.PlaybackState)
+            {
+                mediaplayer.Play();
             }
         }
 
+        private void nextBtn_Click(object sender, RoutedEventArgs e)
+        {
+            playbackList.MoveNext();
+        }
+
+        private void fastForwardBtn_Click(object sender, RoutedEventArgs e)
+        {
+            mediaplayer.PlaybackSession.PlaybackRate += 0.5;
+            Debug.WriteLine($"PlaybackRate : {mediaplayer.PlaybackSession.PlaybackRate}x");
+        }
+
+        private void skipForwardBtn_Click(object sender, RoutedEventArgs e)
+        {
+            mediaplayer.PlaybackSession.Position += TimeSpan.FromSeconds(30);
+        }
+
+        async private void shuffleBtn_Click(object sender, RoutedEventArgs e)
+        {
+            playbackList.ShuffleEnabled = !playbackList.ShuffleEnabled;
+            Debug.WriteLine($"shuffle : {playbackList.ShuffleEnabled}");
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                shuffleBtn.FontWeight =
+                    playbackList.ShuffleEnabled ? Windows.UI.Text.FontWeights.Bold : Windows.UI.Text.FontWeights.Light;
+            });
+        }
+
+       async  private void AutoRepeatBtn_Click(object sender, RoutedEventArgs e)
+        {
+            playbackList.AutoRepeatEnabled = !playbackList.AutoRepeatEnabled;
+            Debug.WriteLine($"AutoRepeat : {playbackList.AutoRepeatEnabled}");
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                autoRepeatBtn.FontWeight =
+                    playbackList.AutoRepeatEnabled ? Windows.UI.Text.FontWeights.Bold : Windows.UI.Text.FontWeights.Light;
+            });
+        }
+
+        private void aspectRatioBtn_Click(object sender, RoutedEventArgs e)
+        {
+            switch (mediaplayerElement.Stretch)
+            {
+                case Stretch.Fill:
+                    mediaplayerElement.Stretch = Stretch.None;
+                    break;
+                case Stretch.None:
+                    mediaplayerElement.Stretch = Stretch.Uniform;
+                    break;
+                case Stretch.Uniform:
+                    mediaplayerElement.Stretch = Stretch.UniformToFill;
+                    break;
+                case Stretch.UniformToFill:
+                    mediaplayerElement.Stretch = Stretch.Fill;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void fullScreenBtn_Click(object sender, RoutedEventArgs e)
+        {
+            mediaplayerElement.IsFullWindow = !mediaplayerElement.IsFullWindow;
+        }
+
+        //skipBackBtn rewindBtn previousBtn playpauseBtn nextBtn fastForwardBtn skipForwardBtn
 
 
 
